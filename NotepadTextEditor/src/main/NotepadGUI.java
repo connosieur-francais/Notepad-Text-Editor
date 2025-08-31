c27061fa3a3530522b5b65a0c8296b4047b3089a
ReasonalWork <reasonalworkplace@gmail.com>
2025-08-31 23:28:15 +0800

    Added KeyBinds

--- NotepadTextEditor/src/main/NotepadGUI.java
+++ NotepadTextEditor/src/main/NotepadGUI.java
@@ -3,28 +3,48 @@ package main;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 
+import javax.swing.AbstractAction;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JToolBar;
+import javax.swing.KeyStroke;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.undo.UndoManager;
 
 public class NotepadGUI extends JFrame {
+	// panel that stores the components
+	private JPanel panel;
+
 	// file explorer
 	private JFileChooser fileChooser;
 
 	private JTextArea textArea;
 	private File currentFile;
+
+	// Swing's built in library to manage undo and redo functionalities
+	private UndoManager undoManager;
+
+	// Swing's built in library to make key bindings
+	private AbstractAction undoAction, redoAction, saveAction, openAction, newAction;
+
+	// Key Bindings that we are Implementing
+	private KeyStroke undoKeyStroke, redoKeyStroke, saveKeyStroke, openKeyStroke, newKeyStroke;
 
 	public NotepadGUI() {
 		super("Notepad");
@@ -32,10 +52,23 @@ public class NotepadGUI extends JFrame {
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+		panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		add(panel, BorderLayout.CENTER);
+
 		// file chooser setup
 		fileChooser = new JFileChooser();
 		fileChooser.setCurrentDirectory(new File(Constants.ASSET_PATH));
 		fileChooser.setFileFilter(new FileNameExtensionFilter("TXT", "txt"));
+
+		undoManager = new UndoManager();
+
+		// key binding setup
+		undoKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_Z, ActionEvent.CTRL_MASK); // Ctrl + Z
+		redoKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_Y, ActionEvent.CTRL_MASK); // Ctrl - Y
+		saveKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK); // Ctrl - S
+		openKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK); // Ctrl - O
+		newKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK); // Ctrl - N
 
 		addGuiComponents();
 	}
@@ -45,7 +78,18 @@ public class NotepadGUI extends JFrame {
 
 		// an area to type text into
 		textArea = new JTextArea();
-		add(textArea, BorderLayout.CENTER);
+		// everytime we text into our JTextArea, undoManager will add an EditEvent
+		// to the undoManager, which will handle the undo and redos
+		textArea.getDocument().addUndoableEditListener(new UndoableEditListener() {
+
+			@Override
+			public void undoableEditHappened(UndoableEditEvent e) {
+				// adds each edit tat we do in the text area (either adding or removing text)
+				undoManager.addEdit(e.getEdit());
+
+			}
+		});
+		panel.add(textArea, BorderLayout.CENTER);
 	}
 
 	private void addToolbar() {
@@ -58,8 +102,10 @@ public class NotepadGUI extends JFrame {
 
 		// add menus
 		menuBar.add(addFileMenu());
+		menuBar.add(addEditMenu());
+		menuBar.add(addFormatMenu());
 
-		add(toolbar, BorderLayout.NORTH);
+		panel.add(toolbar, BorderLayout.NORTH);
 
 	}
 
@@ -68,6 +114,7 @@ public class NotepadGUI extends JFrame {
 
 		// new functionality - resets everything
 		JMenuItem newMenuItem = new JMenuItem("New");
+		newMenuItem.setToolTipText("CTRL + N");
 		newMenuItem.addActionListener(new ActionListener() {
 
 			@Override
@@ -84,8 +131,23 @@ public class NotepadGUI extends JFrame {
 		});
 		fileMenu.add(newMenuItem);
 
+		// define the new action
+		newAction = new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				newMenuItem.doClick();
+			}
+		};
+
+		// associate the ctrl N keystroke with an action name in InputMap
+		panel.getInputMap(JMenuBar.WHEN_IN_FOCUSED_WINDOW).put(newKeyStroke, "NEW");
+
+		// associate action name with the action in the action map
+		panel.getActionMap().put("NEW", newAction);
+
 		// open functionality - open a text file
 		JMenuItem openMenuItem = new JMenuItem("Open");
+		openMenuItem.setToolTipText("CTRL + O");
 		openMenuItem.addActionListener(new ActionListener() {
 
 			@Override
@@ -102,10 +164,10 @@ public class NotepadGUI extends JFrame {
 					if (result != JFileChooser.APPROVE_OPTION || selectedFile == null) {
 						return;
 					}
-					
+
 					// update the currentFile variable
 					currentFile = selectedFile;
-					
+
 					// reset notepad
 					newMenuItem.doClick(); // doClick() method executes the action we implemented with the open feature
 
@@ -133,6 +195,20 @@ public class NotepadGUI extends JFrame {
 			}
 		});
 		fileMenu.add(openMenuItem);
+
+		// define the new action
+		openAction = new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				openMenuItem.doClick();
+			}
+		};
+
+		// associate the ctrl O keystroke with an action name in InputMap
+		panel.getInputMap(JMenuBar.WHEN_IN_FOCUSED_WINDOW).put(openKeyStroke, "OPEN");
+
+		// associate action name with the action in the action map
+		panel.getActionMap().put("OPEN", openAction);
 
 		// save as functionality - creates a new text file and saves user text
 		JMenuItem saveAsMenuItem = new JMenuItem("Save As");
@@ -168,15 +244,14 @@ public class NotepadGUI extends JFrame {
 
 					// update the title header of gui to the save text file
 					setTitle(fileName);
-					
+
 					// update the currentFile variable
 					currentFile = selectedFile;
-					
+
 					// show display dialog
 					JOptionPane.showMessageDialog(NotepadGUI.this, "Saved File!");
-					
+
 				} catch (Exception e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
@@ -185,17 +260,21 @@ public class NotepadGUI extends JFrame {
 
 		// save functionality - saves text into current text file
 		JMenuItem saveMenuItem = new JMenuItem("Save");
+		saveMenuItem.setToolTipText("CTRL + S");
 		saveMenuItem.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// if the current file is null, then we have to perform save as functionality
-				if (currentFile == null) saveAsMenuItem.doClick();
-				
+				if (currentFile == null)
+					saveAsMenuItem.doClick();
+
 				// if the user chooses to cancel saving the file, this means that the
-				// current file will still be null, then we want to prevent executing the rest of the code
-				if (currentFile == null) return;
-				
+				// current file will still be null, then we want to prevent executing the rest
+				// of the code
+				if (currentFile == null)
+					return;
+
 				try {
 					// write to current file
 					FileWriter fileWriter = new FileWriter(currentFile);
@@ -210,6 +289,20 @@ public class NotepadGUI extends JFrame {
 		});
 		fileMenu.add(saveMenuItem);
 
+		// define the new action
+		saveAction = new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				saveMenuItem.doClick();
+			}
+		};
+
+		// associate the ctrl S keystroke with an action name in InputMap
+		panel.getInputMap(JMenuBar.WHEN_IN_FOCUSED_WINDOW).put(saveKeyStroke, "SAVE");
+
+		// associate action name with the action in the action map
+		panel.getActionMap().put("SAVE", saveAction);
+
 		// exit functionality - ends program process
 		JMenuItem exitMenuItem = new JMenuItem("Exit");
 		exitMenuItem.addActionListener(new ActionListener() {
@@ -221,5 +314,86 @@ public class NotepadGUI extends JFrame {
 		fileMenu.add(exitMenuItem);
 
 		return fileMenu;
+	}
+
+	private JMenu addEditMenu() {
+		JMenu editMenu = new JMenu("Edit");
+
+		JMenuItem undoMenuItem = new JMenuItem("Undo");
+		undoMenuItem.setToolTipText("Ctrl + Z");
+		undoMenuItem.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// means that if there are any edits we can undo,
+				// then we undo them
+				if (undoManager.canUndo()) {
+					undoManager.undo();
+				}
+
+			}
+		});
+		editMenu.add(undoMenuItem);
+
+		// define undo action
+		undoAction = new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				undoMenuItem.doClick();
+			}
+		};
+
+		// associate the ctrl z keystroke with an action name in InputMap
+		panel.getInputMap(JMenuBar.WHEN_IN_FOCUSED_WINDOW).put(undoKeyStroke, "UNDO");
+
+		// associate action name with the action in the action map
+		panel.getActionMap().put("UNDO", undoAction);
+
+		JMenuItem redoMenuItem = new JMenuItem("Redo");
+		redoMenuItem.setToolTipText("Ctrl + Y");
+		redoMenuItem.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// means that if there is an edit we can redo, then redo it
+				if (undoManager.canRedo()) {
+					undoManager.redo();
+				}
+			}
+		});
+		editMenu.add(redoMenuItem);
+
+		// define the redo action
+		redoAction = new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				redoMenuItem.doClick();
+			}
+		};
+
+		// associate the ctrl y keystroke with an action name in InputMap
+		panel.getInputMap(JMenuBar.WHEN_IN_FOCUSED_WINDOW).put(redoKeyStroke, "REDO");
+
+		// associate action name with the action in the action map
+		panel.getActionMap().put("REDO", redoAction);
+
+		return editMenu;
+	}
+	
+	private JMenu addFormatMenu() {
+		JMenu formatMenu = new JMenu("Format");
+		
+		// wrap word functionality
+		JCheckBoxMenuItem wordWrapMenuItem = new JCheckBoxMenuItem("Word Wrap");
+		formatMenu.add(wordWrapMenuItem);
+		
+		// aligning text
+		JMenu alignTextMenu = new JMenu("Align Text");
+		formatMenu.add(alignTextMenu);
+		
+		// font format
+		JMenuItem fontMenuItem = new JMenuItem("Font...");
+		formatMenu.add(fontMenuItem);
+		return formatMenu;
 	}
 }
